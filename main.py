0f8d64dc8fa070a234ea2ffadeaf8c1a0c5c1618
savyon <mmseidenwar@gmail.com>
2025-09-08 14:18:12 +0300

    commit1

---
+++ main.py
@@ -0,0 +1,12 @@
+#1
+print(" Hello Enter your  name: Zoe")
+
+#2
+print(" Hello Enter your adress: hasfira 11 pardes hana")
+
+
+
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
